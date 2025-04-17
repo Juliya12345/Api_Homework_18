4f18c3464da8c20343c67b4fb31e6f7f06f2a4aa
Kremleva Yulia <rovenor@gmail.com>
2025-04-17 21:18:39 +0300

    add propirtes

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -4,11 +4,11 @@ import models.BookModel;
 import models.CredentialsModel;
 
 public class TestData {
-    public static String login = "JuliaTest1",
-                         password = "JuliaTest1/!",
+    public static String LOGIN = System.getProperty("LOGIN"),
+            PASSWORD  = System.getProperty("PASSWORD"),
                          isbn = "9781449365035",
                          title = "Speaking JavaScript";;
 
-    public static CredentialsModel credentials = new CredentialsModel(login, password);
+    public static CredentialsModel credentials = new CredentialsModel(LOGIN, PASSWORD);
     public static BookModel book = new BookModel(isbn, title);
 }
